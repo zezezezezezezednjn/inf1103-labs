b205c3b2fca52d879471f85274e72635b54e7f94
Zi En <2602960@sit.singaporetech.edu.sg>
2026-09-04 11:08:57 +0800

    activity 2

--- hello.py
+++ hello.py
@@ -1,3 +1,31 @@
 print("=============")
-print("jdfijfijf")
+print("Welcome here")
+print("First post here")
 print ("============")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+username = input("Enter username: ")
+age = input("Enter age: ")
+category = input("Enter content category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("username:", username)
+print("age:", age)
+print("category:", category)
