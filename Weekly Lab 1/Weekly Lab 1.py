f62b4466661552503bcc63879f7f751e90db43ab
Zi En <2602960@sit.singaporetech.edu.sg>
2026-09-04 11:11:53 +0800

    activity 5

--- Weekly Lab 1/Weekly Lab 1.py
+++ Weekly Lab 1/Weekly Lab 1.py
@@ -20,3 +20,15 @@ print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
 
+username = input("Enter username: ")
+age =int(input("Enter age: "))
+category = input("Enter content category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("username:", username)
+print("age:", age)
+print("category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you???")
---
+++ Weekly Lab 2/auditor.py
@@ -0,0 +1,10 @@
+Inventory = 0
+while True:
+    stock = input ("Enter the stock quantity or type 'quit ' to exit:")
+    if stock == "quit":
+        print ("quit")
+        break
+    
+
+
+        
