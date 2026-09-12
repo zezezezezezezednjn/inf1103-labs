3cb3729101dcc7e97778629fcf3836dc03b844b7
Zi En <2602960@sit.singaporetech.edu.sg>
2026-09-12 14:27:36 +0800

    complete

--- Weekly Lab 2/auditor.py
+++ Weekly Lab 2/auditor.py
@@ -1,10 +1,29 @@
 Inventory = 0
-while True:
-    stock = input ("Enter the stock quantity or type 'quit ' to exit:")
-    if stock == "quit":
-        print ("quit")
-        break
-    
+Failed_Entry = 0
 
+while True:
+    stock = input ("Enter the stock quantity or type 'quit' to exit: ")
+    if stock == "quit":
+        print ("Exiting system...")
+        print("Total Units Proccessed: ",Inventory)
+        print("Failed Entries: ",Failed_Entry)
+        break
+
+    elif stock.startswith("-"):
+        print ("ERROR: No negative numbers allowed.")
+        Failed_Entry += 1
+        continue
+
+    elif stock.isdigit() == False:
+            print ("ERROR: Please enter a valid number.")
+            Failed_Entry += 1
+            continue
+    
+    stock = int(stock)
+    Inventory += int(stock)
+
+    if Inventory > 500:
+         print ("ALERT: Inventory exceeds 500 units.")
+         break
 
         
